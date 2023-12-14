497e37de0db6d811e8d2cb062ceedfcd1633872d
Amir-c-137 <trool.usiz@gmail.com>
2023-12-14 16:43:45 +0330

    add navbar

--- dist/script.js
+++ dist/script.js
@@ -8,3 +8,25 @@ function closeMenu() {
     sideMenu.classList.remove('left-0');
     sideMenu.classList.add('left-[-250px]');
 }
+
+function showContent(contentId) {
+    // Hide all content divs
+    const allContent = document.querySelectorAll('.content');
+    allContent.forEach(content => content.classList.add('hidden'));
+
+    // Show the selected content
+    const selectedContent = document.getElementById(`${contentId}Content`);
+    if (selectedContent) {
+      selectedContent.classList.remove('hidden');
+    }
+
+    // Remove the blue border from all menu items
+    const allMenuItems = document.querySelectorAll('#menu a');
+    allMenuItems.forEach(item => item.classList.remove('border-b-2', 'border-blue-500'));
+
+    // Add the blue border to the selected menu item
+    const selectedMenuItem = document.querySelector(`#menu a[href="#"][onclick="showContent('${contentId}')"]`);
+    if (selectedMenuItem) {
+      selectedMenuItem.classList.add('border-b-2', 'border-blue-500');
+    }
+  }
