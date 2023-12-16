86868843c408c4b5fa83660f59216a174ae4efb2
Amir-c-137 <trool.usiz@gmail.com>
2023-12-16 15:31:04 +0330

    add about us div

--- dist/script.js
+++ dist/script.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
   // This code runs when the DOM is fully loaded
 
-  // Add the blue underline to the "Home" menu item by default
-  const defaultMenuItem = document.querySelector('#menu a[onclick="showContent(\'home\')"]');
+  // Add the blue underline to the "about us" menu item by default
+  const defaultMenuItem = document.querySelector('#menu a[onclick="showContent(\'about\')"]');
   if (defaultMenuItem) {
     defaultMenuItem.classList.add('border-b-2', 'border-blue-500');
   }
@@ -28,4 +28,14 @@ function showContent(contentId) {
   if (selectedMenuItem) {
     selectedMenuItem.classList.add('border-b-2', 'border-blue-500');
   }
+}
+
+function openMenu() {
+  const sideMenu = document.getElementById('side-menu');
+  sideMenu.style.left = '0';
+}
+
+function closeMenu() {
+  const sideMenu = document.getElementById('side-menu');
+  sideMenu.style.left = '-250px';
 }
